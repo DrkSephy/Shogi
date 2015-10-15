98364930cef5660580138ec88f0c6b9a107a7c57
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-15 17:11:24 -0400

    Generating simple game board

--- animal/game.py
+++ animal/game.py
@@ -3,7 +3,11 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def board():
+	board = []
+	for i in range(0, 4):
+		board.append([-1, -1, -1, -1])
+	print board
 	return "Hello World!"
 
 if __name__ == "__main__":
