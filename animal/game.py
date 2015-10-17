5fcb64339173b1093ffd194009cd12063c72654e
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-16 21:09:03 -0400

    Refactoring game constants

--- animal/game.py
+++ animal/game.py
@@ -10,20 +10,5 @@ def game():
 	helpers.printBoard(board)
 	return "Hello World!"
 
-####################
-# HELPER CONSTANTS #
-####################
-
-# Pieces can move one tile in these directions
-N, S, W, E, NW, NE, SW, SE = 1, 1, 1, 1, 1, 1, 1, 1
-
-pieces = {
-    'C' : (N),
-    'H' : (N, S, W, E, NW, NE),
-	'E' : (NW, NE, SW, SE),
-	'G' : (N, S, W, E),
-	'L' : (N, S, W, E, NW, NE, SW, SE)
-}
-
 if __name__ == "__main__":
 	app.run(debug=True)
