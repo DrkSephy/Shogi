2ee63546e0d34d8c8dde4e8d0c52c80e8f40402e
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-16 20:19:50 -0400

    Added rest of Shogi pieces

--- animal/game.py
+++ animal/game.py
@@ -13,9 +13,12 @@ def board():
 ####################
 # HELPER CONSTANTS #
 ####################
+
+# Pieces can move one tile in these directions
 N, S, W, E, NW, NE, SW, SE = 1, 1, 1, 1, 1, 1, 1, 1
 pieces = {
 	'C' : N, 
+	'H' : N, S, W, E, NW, NE,
 	'E' : NW, NE, SW, SE,
 	'G' : N, S, W, E,
 	'L' : N, S, W, E, NW, NE, SW, SE
