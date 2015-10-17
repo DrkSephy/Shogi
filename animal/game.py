e47940da9b05672e4f4ce857aabcfe769fc3cc92
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-16 21:46:57 -0400

    Adding barebones game template

--- animal/game.py
+++ animal/game.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template
 import helpers
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ def game():
 	board = helpers.generateBoard(0, 4)
 	helpers.generatePieces(board)
 	helpers.printBoard(board)
-	return "Hello World!"
+	return render_template("game.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
