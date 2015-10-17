753b06505e75d87797cd077b9dc7f4d7620f59a7
DrkSephy <dleonar00@citymail.cuny.edu>
2015-10-17 18:17:35 -0400

    Documenting existing JavaScript, code cleanup

--- animal/js/main.js
+++ animal/js/main.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-	/* Set up the board */
-	var row = 0;
-	var col = 0;
-	$('.square').each(function() {
-		$(this).data('row', row);
-		$(this).data('col', col);
-		col++;
-		if(col > 2) {
-			row++;
-			col = 0;
-		}
-	});
 
+	// Generate rows/column numbers
+	generateBoard(0, 0);
+
+	/**
+	 * Attaches row/column data to existing DOM elements.
+	 * @param {number} row The number of rows to create.
+	 * @param {number} col The number of columns to create.
+	*/ 
+	function generateBoard(row, col) {
+		$('.square').each(function() {
+			$(this).data('row', row);
+			$(this).data('col', col);
+			col++;
+			if(col > 2) {
+				row++;
+				col = 0;
+			}
+		});
+	}
+	
 	$('.square').click(function() {
-		console.log($(this).data('row'));
-		console.log($(this).data('col'));
+		console.log('Row: ' + $(this).data('row') + ' , ' + 'Col: ' + $(this).data('col'));
 	})
 });
